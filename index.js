9d486aa542c3684bbd5f641c6004e0eeb939cdfd
Gabriel Schuster <info@actra.de>
2017-06-27 23:14:15 +0200

    Updated to redux-persist-transform-filter 0.0.13

--- index.js
+++ index.js
@@ -1,32 +1,68 @@
 import { Iterable, Map } from 'immutable';
 import { createTransform } from 'redux-persist';
 import get from 'lodash.get';
+import includes from 'lodash.includes';
+import isObject from 'lodash.isobject';
+import isString from 'lodash.isstring';
+import isUndefined from 'lodash.isundefined';
 import set from 'lodash.set';
-import isString from 'lodash.isString';
-import isUndefined from 'lodash.isUndefined';
+import unset from 'lodash.unset';
 
 
-export default function createFilter(reducerName, inboundPaths, outboundPaths) {
+export function createFilter(reducerName, inboundPaths, outboundPaths, transformType = 'whitelist') {
+    transformType = includes(['whitelist', 'blacklist'], transformType) ? transformType : 'whitelist';
+    
     return createTransform(
-        (inboundState, key) => inboundPaths ? persistFilter(inboundState, inboundPaths) : inboundState,
-        (outboundState, key) => outboundPaths ? persistFilter(outboundState, outboundPaths) : outboundState,
+        (inboundState, key) => inboundPaths ? persistFilter(inboundState, inboundPaths, transformType) : inboundState,
+        (outboundState, key) => outboundPaths ? persistFilter(outboundState, outboundPaths, transformType) : outboundState,
         {whitelist: [reducerName]}
     );
 };
 
 
-export function persistFilter(state, paths = []) {
-    let iterable = Iterable.isIterable(state);
-    let subset   = iterable ? Map({}) : {};
+export function createWhitelistFilter(reducerName, inboundPaths, outboundPaths) {
+    return createFilter(reducerName, inboundPaths, outboundPaths, 'whitelist');
+}
+
+
+export function createBlacklistFilter(reducerName, inboundPaths, outboundPaths) {
+    return createFilter(reducerName, inboundPaths, outboundPaths, 'blacklist');
+}
+
+
+function filterObject({path, filterFunction = () => true}, state) {
+    return pickBy(get(state, path), filterFunction);
+}
+
+
+export function persistFilter(state, paths = [], transformType = 'whitelist') {
+    if(!includes(['whitelist', 'blacklist'], transformType)) {
+        return state;
+    }
+    
+    let blacklist = ('blacklist' === transformType);
+    let iterable  = Iterable.isIterable(state);
+    let subset    = iterable ? Map(blacklist ? state : {}) : (blacklist ? state : {});
     
     (isString(paths) ? [paths] : paths).forEach((path) => {
-        let key   = isString(path) ? [path] : path;
-        let value = iterable ? state.getIn(key) : get(state, key);
+        let key         = isString(path) ? [path] : path;
+        let keyIsObject = (isObject(key) && !Array.isArray(key) && key.hasOwnProperty('path'));
+        let value       = iterable ? state.getIn(key) : (keyIsObject ? filterObject(key, state) : get(state, key));
         
         if(!isUndefined(value)) {
-            iterable ? (subset = subset.setIn(key, value)) : set(subset, key, value);
+            iterable ?
+                (subset = blacklist ? subset.deleteIn(key) : subset.setIn(key, value))
+                :
+                (blacklist ?
+                    (keyIsObject ? forIn(value, (iterateeValue, iterateeKey) => { unset(subset, `${key.path}.${iterateeKey}`) })
+                    :
+                    unset(subset, path)) : set(subset, keyIsObject ? key.path : key, value)
+                );
         }
     });
 
     return subset;
 }
+
+
+export default createFilter;
